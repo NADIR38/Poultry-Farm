ce49ca0ee52b6ebbc6a12a9188c85503411a3a8f
Javairia Qasim <javairia.professional@gmail.com>
2025-08-10 16:38:57 +0500

    Updated-Staff

--- sample/Models/Staffs.cs
+++ sample/Models/Staffs.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace pro.BL.Model
 {
     public class Staffs
     {
         public int StaffID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Contact is required")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "Contact must be exactly 11 digits")]
         public string Contact { get; set; }
+
+        [Required(ErrorMessage = "Role is required")]
+        [RegularExpression("^(Admin|Employee)$", ErrorMessage = "Role must be either Admin or Employee")]
         public string Role { get; set; }
+
+        [Required(ErrorMessage = "CNIC is required")]
+        [RegularExpression(@"^\d{13}$", ErrorMessage = "CNIC must be exactly 13 digits")]
         public string CNIC { get; set; }
 
         public Staffs() { }
